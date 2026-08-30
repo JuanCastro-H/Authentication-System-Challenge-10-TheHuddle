5ef183a60098fc75151609f9d032d95c9fecb139
Juan Castro <juamcastroh@gmail.com>
2026-08-30 02:58:58 -0300

    faet: Crear logica para arrancar el servidor

---
+++ src/server.js
@@ -0,0 +1,13 @@
+// --- Cargar Variables De Entorno ---
+require("dotenv").config();
+
+// --- Imoprtar Configuracion De La Aplicacions ---
+const app = require("./app");
+
+// --- Definir Puerto ---
+const PORT = process.env.PORT || 3000;
+
+//  --- Iniciar Servidor ---
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
