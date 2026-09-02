c455bb463271ebabf0ce292e8cea451919dcd4fc
Juan Castro <juamcastroh@gmail.com>
2026-09-02 17:27:26 -0300

    feat: Agregar ruta de login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,7 +4,19 @@ const { register } = require("../controllers/auth.controller");
 
 const router = express.Router();
 
+
+// ---------------------------
+// REGISTRO
 // Ruta tipo POST/register
+// ---------------------------
 router.post("/register", register);
+
+
+// ---------------------------
+// LOGIN
+// Ruta tipo POST/login
+// ---------------------------
+
+router.post("/login", login);
 
 module.exports = router;
