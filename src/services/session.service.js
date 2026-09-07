838e742543c482331ac17bf9f8d7364f7204164b
Juan Castro <juamcastroh@gmail.com>
2026-09-07 15:57:21 -0300

    feat: Crear funcion para crear un indentificador temporal para iniciar a session y poder crear el JWT

--- src/services/session.service.js
+++ src/services/session.service.js
@@ -5,7 +5,7 @@
 // --- Importar Libreria Cryptografica ---
 const crypto = require("crypto");
 
-// --- Convierte Token En Texto Plano En Un Hash ---
+// --- Hashea El JWT ---
 const hashToken = (token) => {
 
     return crypto
@@ -14,6 +14,16 @@ const hashToken = (token) => {
         .digest("hex");       // Genera el resultado final en una cadena Hexadecimal.
 };
 
+
+// --- Generar Identificador Temporal Para La Session  ---
+const generateSessionPlaceholder = () => {
+    return crypto.randomBytes(32).toString("hex"); // Genera un valor temporal para iniciar la sesion.
+                                                   // Despues lo reemplazamos por el hash real del JWT.
+}
+
+
+// --- Exportar Funciones Del Modulo ---
 module.exports = {
-    hashToken
+    hashToken,
+    generateSessionPlaceholder
 };
